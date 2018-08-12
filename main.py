f8884cb326498594dd3f4d791a78b97a4491f51f
Daniil Tsarkov <dankan_tsar@protonmail.ch>
2018-08-12 15:38:39 +0300

    Task #2 	- image now loadig from f/s and drawing on canvas

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import cv2
-from tkinter import *
 import numpy as np
-from PIL import ImageTk, Image
+from tkinter import *
 from tkinter.filedialog import askopenfilename
+from PIL import Image, ImageTk
 
-class Hsv():
+
+class Hsv:
     def __init__(self):
         self.h = 0
         self.s = 0
@@ -23,40 +24,63 @@ class Hsv():
         update_mask()
 
     def get(self):
-        return (self.h, self.s, self.v)
+        return self.h, self.s, self.v
+
 
 def update_mask():
-    
+    img = cv2.imread(window.path)
+    print(window.path)
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV )
     h_min = np.array(hsv1.get(), np.uint8)
     h_max = np.array(hsv2.get(), np.uint8)
     raw_mask = cv2.inRange(hsv, h_min, h_max)
     mask = cv2.bitwise_and(hsv, hsv, mask=raw_mask)
-    res = ImageTk.PhotoImage(Image.fromarray(mask))
+    window.update(mask)
     print(hsv1.get(), hsv2.get())
-    can.itemconfig(area, image = res)
+
+
+class MainWindow:
+    def __init__(self):
+        self.view = None
+        self.choose = None
+        self.image = None
+
+        self.build_widgets()
+
+    def build_widgets(self):
+        self.view = Canvas(settings, width=300, height=300, bg="white")
+        self.view.pack()
+        s_h1 = Scale(settings, from_=0, to=255, orient=HORIZONTAL, command=hsv1.upd_h)
+        s_h1.pack()
+        s_s1 = Scale(settings, from_=0, to=255, orient=HORIZONTAL, command=hsv1.upd_s)
+        s_s1.pack()
+        s_v1 = Scale(settings, from_=0, to=255, orient=HORIZONTAL, command=hsv1.upd_v)
+        s_v1.pack()
+        s_h2 = Scale(settings, from_=0, to=255, orient=HORIZONTAL, command=hsv2.upd_h)
+        s_h2.set(255)
+        s_h2.pack()
+        s_s2 = Scale(settings, from_=0, to=255, orient=HORIZONTAL, command=hsv2.upd_s)
+        s_s2.set(255)
+        s_s2.pack()
+        s_v2 = Scale(settings, from_=0, to=255, orient=HORIZONTAL, command=hsv2.upd_v)
+        s_v2.set(255)
+        s_v2.pack()
+
+        choose = Button(settings, text="Open", command=self.choose_file)
+        choose.pack()
+
+    def update(self, mask):
+        self.image = ImageTk.PhotoImage(Image.fromarray(mask))
+        self.view.create_image(0, 0, image=self.image, anchor="center")
+
+    def choose_file(self):
+        self.path = askopenfilename()
+        self.image = ImageTk.PhotoImage(Image.open(self.path))
+        self.view.create_image(0, 0, image=self.image, anchor="center")
+
 
 hsv1 = Hsv()
 hsv2 = Hsv()
-path = askopenfilename()
-img = cv2.imread(path)
 settings = Tk()
-can = Canvas()
-can.pack(side='top', fill='both', expand='yes')  
-area = can.create_image(0, 0, image=ImageTk.PhotoImage(file=path), anchor='nw')
-s_h1 = Scale(settings, from_=0, to=255, orient=HORIZONTAL, command=hsv1.upd_h)
-s_h1.pack()
-s_s1 = Scale(settings, from_=0, to=255, orient=HORIZONTAL, command=hsv1.upd_s)
-s_s1.pack()
-s_v1 = Scale(settings, from_=0, to=255, orient=HORIZONTAL, command=hsv1.upd_v)
-s_v1.pack()
-s_h2 = Scale(settings, from_=0, to=255, orient=HORIZONTAL, command=hsv2.upd_h)
-s_h2.pack()
-s_s2 = Scale(settings, from_=0, to=255, orient=HORIZONTAL, command=hsv2.upd_s)
-s_s2.pack()
-s_v2 = Scale(settings, from_=0, to=255, orient=HORIZONTAL, command=hsv2.upd_v)
-s_v2.pack()
+window = MainWindow()
 settings.mainloop()
-
-
-
